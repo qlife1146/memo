f05ccbaa04a6d554342de1ceda2631d8c47ede5f
Luca Park <qlife1146@gmail.com>
2025-05-14 17:31:57 +0900

    Initial Commit

---
+++ memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  memo
+//
+//  Created by Luca Park on 5/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
