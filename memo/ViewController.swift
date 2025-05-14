b03dbc5a3ef291043f584a889e8c43eb8ea2bc43
Luca Park <qlife1146@gmail.com>
2025-05-14 18:30:44 +0900

    일단 생성.. 따라 만든 거라 연구 필요

--- memo/ViewController.swift
+++ memo/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var tableView: UITableView!
+
+    var memoList: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        for i in 0..<20 {
+            memoList.append("\(i)")
+        }
     }
 
+    @IBAction func addMemo(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add Memo", message: "Type a memo", preferredStyle: .alert)
+        alert.addTextField()
 
+        let addAction = UIAlertAction(title: "Add", style: .default) { _ in
+            if let text = alert.textFields?.first?.text, !text.isEmpty {
+                self.memoList.append(text)
+                self.tableView.reloadData()
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+
+        alert.addAction(addAction)
+        alert.addAction(cancelAction)
+
+        present(alert, animated: true)
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memoList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = memoList[indexPath.row]
+        return cell
+    }
 }
-
